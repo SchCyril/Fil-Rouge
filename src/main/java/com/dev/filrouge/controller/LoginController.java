bb6d38a83ccb6e9d14b8f6caa663ea02ffcdefd6
Baptiste <smaug.exe@gmail.com>
2019-02-26 17:50:03 +0100

    test

---
+++ src/main/java/com/dev/filrouge/controller/LoginController.java
@@ -0,0 +1,53 @@
+package com.dev.filrouge.controller;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+import com.dev.filrouge.model.User;
+import com.dev.filrouge.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("/login")
+public class LoginController {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepo ur;
+
+    @PostMapping
+    public void createUser(@RequestBody Map<String, String> action) {
+        ur.save(new User(action.get("login"), passwordEncoder.encode(action.get("mdp")), action.get("name"), action.get("role")));
+    }
+
+    @RequestMapping(value="user", method=RequestMethod.GET)
+    public String getConnectedUser(Principal principal) {
+        if (principal != null) {
+            return principal.getName();
+        }
+        return null;
+    }
+    
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping
+    public ResponseEntity<?> getMethodName() {
+        System.out.println("AUTORITY");
+        return ResponseEntity.ok().build();
+    }
+    
+}
