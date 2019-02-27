ed551f43252ca4a8e401dd3cc889c5cd6066084e
Cyril <schneidenbach.cyril@gmail.com>
2019-02-27 12:22:12 +0100

    hesih

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -4,7 +4,8 @@ import { NgbDropdown, NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
+  providers:[NgbDropdownConfig]
 })
 export class MenuComponent implements OnInit {
 
