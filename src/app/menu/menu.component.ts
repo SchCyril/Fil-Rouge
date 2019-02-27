edf4ee60737f2a4339967fdd7b5b4c8b442bc78b
Cyril <schneidenbach.cyril@gmail.com>
2019-02-27 12:19:41 +0100

    test

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbDropdown, NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbDropdownConfig) {
+    config.placement = "bottom";
+    config.autoClose = true;
+   }
 
   ngOnInit() {
   }
