9aabb93affd0984dd6715aaa1de337b48bf3e5f5
benjamin <benjam.montet@gmail.com>
2019-02-27 11:30:31 +0100

    Navbar Admin model OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { MenuAdminComponent } from './menu-admin/menu-admin.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { AccueilAdminComponent } from './accueil-admin/accueil-admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuAdminComponent
+    MenuAdminComponent,
+    DropdownComponent,
+    AccueilAdminComponent
   ],
   imports: [
     BrowserModule,
